f46c0feb71d986ab6c4b7178a43208e01ec78bee
Sachini Dissanayaka <47107946+Sachini-Dissanayaka@users.noreply.github.com>
2019-12-24 14:44:30 +0530

    product basic

---
+++ src/Form/OrdersType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Orders;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrdersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+            ->add('date_placed')
+
+            ->add('customer')
+            ->add('route')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Orders::class,
+        ]);
+    }
+}
