88751d91cfa3e6f4a93768004c36360759902eba
Yoshani Ranaweera <yoshani.malinka@gmail.com>
2019-12-29 17:21:15 +0530

    driver_assistant

--- src/Repository/DriverAssistantRepository.php
+++ src/Repository/DriverAssistantRepository.php
@@ -89,5 +89,15 @@ class DriverAssistantRepository extends ServiceEntityRepository implements Passw
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
+    public function changeAvailability($status, $id){
+        $conn= $this->getEntityManager()->getConnection();
+        $sql = "UPDATE driver_assistant SET status=? WHERE id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(1, $status);
+        $stmt->bindParam(2, $id);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
     
 }
